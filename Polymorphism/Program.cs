0ce217f36bb8d183239dd81922c1c9e6c257a6a4
Khilchenko Yurii <yu.kh414@gmail.com>
2025-01-09 21:17:36 +0200

    formatting namespace and add access modifier

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,58 +1,57 @@
-﻿namespace Polymorphism
+﻿namespace Polymorphism;
+
+public class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            CheckDbCommand();
-        }
+        CheckDbCommand();
+    }
 
-        public static void CheckDbCommand()
-        {
-            var sqlDb = new SqlConnection("Sql connection");
+    public static void CheckDbCommand()
+    {
+        var sqlDb = new SqlConnection("Sql connection");
 
-            var oracleDb = new OracleConnection("Oracle connection");
+        var oracleDb = new OracleConnection("Oracle connection");
 
-            var dbCommands = new DbCommand(sqlDb);
+        var dbCommands = new DbCommand(sqlDb);
 
-            dbCommands.Execute("SELECT *");
+        dbCommands.Execute("SELECT *");
 
-            dbCommands = new DbCommand(oracleDb);
+        dbCommands = new DbCommand(oracleDb);
 
-            dbCommands.Execute("SELECT *");
+        dbCommands.Execute("SELECT *");
 
-            //dbCommands.Execute("");
+        //dbCommands.Execute("");
 
-            //dbCommands = new DbCommand(null);
-        }
+        //dbCommands = new DbCommand(null);
+    }
 
-        public static void CheckDbConnection()
-        {
-            var sqlDb = new SqlConnection("Sql connection");
+    public static void CheckDbConnection()
+    {
+        var sqlDb = new SqlConnection("Sql connection");
 
-            var oracleDb = new OracleConnection("Oracle connection");
+        var oracleDb = new OracleConnection("Oracle connection");
 
-            sqlDb.OpenConnection();
+        sqlDb.OpenConnection();
 
-            oracleDb.OpenConnection();
+        oracleDb.OpenConnection();
 
-            sqlDb.CloseConnection();
+        sqlDb.CloseConnection();
 
-            oracleDb.CloseConnection();
+        oracleDb.CloseConnection();
 
-        }
+    }
 
-        public static void CheckShapes()
-        {
-            var shapes = new List<Shape>();
+    public static void CheckShapes()
+    {
+        var shapes = new List<Shape>();
 
-            shapes.Add(new Circle());
-            shapes.Add(new Rectangle());
-            shapes.Add(new Triangle());
+        shapes.Add(new Circle());
+        shapes.Add(new Rectangle());
+        shapes.Add(new Triangle());
 
-            Canvas canvas = new Canvas();
+        Canvas canvas = new Canvas();
 
-            canvas.DrawShape(shapes);
-        }
+        canvas.DrawShape(shapes);
     }
 }
