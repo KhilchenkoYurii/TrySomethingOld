771450ae91fe85db17af3f1a431ae7a181d00129
Khilchenko Yurii <yu.kh414@gmail.com>
2025-01-16 20:42:36 +0200

    cover Nullable Types lecture

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MyNamespace;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CheckNullable();
+    }
+
+    public static void CheckNullable()
+    {
+        DateTime? date = new DateTime(2025,1,1);
+
+        DateTime date2 = date ?? DateTime.Today;
+
+        Console.WriteLine(date2);
+
+        Console.WriteLine($"GetValueOrDefault: {date.GetValueOrDefault()}");
+        Console.WriteLine($"HasValue: {date.HasValue}");
+        Console.WriteLine($"Value: {date.Value}");
+    }
+}
