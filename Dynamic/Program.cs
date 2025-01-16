b69ca7beeb088dbbf91e3027e08fdb2bba6e3544
Khilchenko Yurii <yu.kh414@gmail.com>
2025-01-16 20:56:55 +0200

    cover Dynamic lecture

---
+++ Dynamic/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Dynamic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CheckDynamicBindig();
+    }
+
+    public static void CheckDynamicBindig()
+    {
+        object obj = "Yurii";
+
+        //var methodInfo = obj.GetType().GetMethod("GetHashCode");
+        
+        //methodInfo.Invoke(null,null);
+
+        dynamic excelObj = "Yurii";
+        
+        //excelObj.Optimize();
+
+        dynamic name = "Yurii";
+
+        name = 10;
+
+        dynamic a = 10;
+        dynamic b = 10;
+
+        var c = a + b; // c will be dynamic too
+
+        int i = 5;
+
+        dynamic dynamicI = i;
+
+        long l = dynamicI;
+    }
+}
