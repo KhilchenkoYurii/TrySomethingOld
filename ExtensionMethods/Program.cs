07b837dc4c408bbf8d2150e3944e853342354f62
Khilchenko Yurii <yu.kh414@gmail.com>
2025-01-16 20:02:21 +0200

    cover ExtensionMethods lecture

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtensionMethods;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CheckStringExtension();
+    }
+
+    public static void CheckStringExtension()
+    {
+        string post = "This is a very long post about some shit...";
+
+        Console.WriteLine(post.Shorten(5));
+    }
+}
---
+++ ExtensionMethods/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtensionMethods;
+
+public static class StringExtension
+{
+    public static string Shorten(this String str, int numbersOfWords)
+    {
+        if (numbersOfWords <= 0)
+        {
+            return "";
+        }
+
+        var words = str.Split(' ');
+
+        if (words.Length < numbersOfWords)
+        {
+            return str;
+        }
+
+        return string.Join(" ", words.Take(numbersOfWords));
+    }
+}
